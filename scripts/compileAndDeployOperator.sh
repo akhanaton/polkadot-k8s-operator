df6a61a7e3c081bbd93126898f26bd51a7ea7f25
Onori Alessio, SUB-BLO (EXT) <Alessio.Onori@swisscom.com>
2020-02-17 15:29:44 +0100

    added networkpolicies to Kind role in /deployment/role.yaml

--- scripts/compileAndDeployOperator.sh
+++ scripts/compileAndDeployOperator.sh
@@ -1,5 +1,5 @@
 pushd ..
-operator-sdk build ironoa/customresource-operator:v0.0.6 # define your favourite
-docker push ironoa/customresource-operator:v0.0.6 #define your favourite
+operator-sdk build ironoa/customresource-operator:v0.0.7 # define your favourite
+docker push ironoa/customresource-operator:v0.0.7 #define your favourite
 kubectl create -f deploy/operator.yaml
 popd
